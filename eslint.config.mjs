902a6b292aa72cae9f2f1ec34b199debf11d4e94
AminManafzadeh <amin.mah1375@gmail.com>
2025-09-14 03:59:46 -0700

    change some config part 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +10,34 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // تنظیمات پیش‌فرض Next.js
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  // Ignore مسیرها
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
+  },
+
+  // قوانین عمومی پروژه
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+
+  // غیرفعال‌سازی rule خاص برای یک فایل
+  {
+    files: ['next-env.d.ts'],
+    rules: {
+      '@typescript-eslint/triple-slash-reference': 'off',
+    },
   },
 ];
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
+  console.log('test');
+  const renderName = () => {
+    const name = 'Amin';
+    return <span>{name}</span>;
+  };
   return (
     <div>
       <h1 className="font-bold text-blue-600">Hi</h1>
+      {renderName()}
     </div>
   );
 }
